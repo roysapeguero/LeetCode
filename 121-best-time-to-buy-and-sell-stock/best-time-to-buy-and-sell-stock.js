b4c4bfcf054a16eb7cde86a7d8978cd565bc145f
Roysa Peguero Martinez <110946315+roysapeguero@users.noreply.github.com>
2023-12-22 23:44:36 -0800

    Time: 74 ms (47.33%) | Memory: 51.3 MB (86.20%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let l = 0, r = 1, maxProfit = 0
+
+    while (r < prices.length) {
+        if (prices[l] < prices[r]) {
+            maxProfit = Math.max(prices[r] - prices[l], maxProfit)
+        } else l = r
+        r++
+    }
+    return maxProfit
+};
