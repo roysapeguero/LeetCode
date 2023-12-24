f0f95df530113d78b16245577c3a4f0b0ace3f82
Roysa Peguero Martinez <110946315+roysapeguero@users.noreply.github.com>
2023-12-24 00:24:57 -0800

    Time: 57 ms (63.52%) | Memory: 44.4 MB (29.40%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+    let hash = {}
+    let maxPal = 0
+
+    
+    for (let char of s) {
+        if (hash[char] !== undefined) hash[char] += 1
+        else hash[char] = 1
+        
+        if (hash[char] % 2 === 0) maxPal += 2
+    }  
+
+    return s.length > maxPal ? maxPal + 1 : maxPal
+};
