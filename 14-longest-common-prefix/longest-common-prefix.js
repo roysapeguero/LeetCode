6af9ca134a15cd9013dfd9b5c29b4c715f2ab8bb
Roysa Peguero Martinez <110946315+roysapeguero@users.noreply.github.com>
2023-12-24 22:44:26 -0800

    Time: 49 ms (83.69%) | Memory: 42.6 MB (35.56%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -5,8 +5,10 @@
 var longestCommonPrefix = function(strs) {
     const testStr = strs[0];
 
-    for (let i in testStr) {
-        if (!strs.every(str => str[i] === testStr[i])) return testStr.slice(0, i);
+    for (let i = 0; i < testStr.length; i++) {
+        for (let j = 1; j < strs.length; j++) {
+            if (strs[j][i] !== testStr[i]) return testStr.slice(0, i);
+        }
     }
     return testStr;
 };
