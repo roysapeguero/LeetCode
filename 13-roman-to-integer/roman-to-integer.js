a2c8e8cef38426eafdc67f28c043b3bdfadfa672
Roysa Peguero Martinez <110946315+roysapeguero@users.noreply.github.com>
2023-12-27 00:02:21 -0800

    Time: 111 ms (49.26%) | Memory: 46.9 MB (73.85%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.js
+++ 13-roman-to-integer/roman-to-integer.js
@@ -3,19 +3,23 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    let symbols = {
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000
-    };
-
-    value = 0;
-    for(let i = 0; i < s.length; i+=1){
-        symbols[s[i]] < symbols[s[i+1]] ? value -= symbols[s[i]]: value += symbols[s[i]]
+    let total = 0
+    let key = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
     }
-    return value
+
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i]
+        if (key[char] < key[s[i + 1]]) { 
+            total += key[s[i+1]] - key[char] 
+            i++
+        } else total += key[char]
+    } 
+    return total
 };
