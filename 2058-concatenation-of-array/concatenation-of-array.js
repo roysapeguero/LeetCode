33b357ef1ea2e98142932d3d6b94f5039665fdd4
Roysa Peguero Martinez <110946315+roysapeguero@users.noreply.github.com>
2024-02-13 07:47:47 -0800

    Time: 69 ms (47.13%) | Memory: 51.6 MB (30.38%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.js
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -3,11 +3,5 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    let ans = []
-
-    for (let i = 0; i < nums.length; i ++) {
-        ans[i] = nums[i]
-        ans[i + nums.length] = nums[i]
-    }
-    return ans
+    return [...nums, ...nums]
 };
