0d19cb2b1c5b32734a0dc7b20e7e07444ab3301d
Roysa Peguero Martinez <110946315+roysapeguero@users.noreply.github.com>
2024-02-18 19:00:33 -0800

    Time: 57 ms (99.53%) | Memory: 55.1 MB (6.15%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    // create left and right pointers
+    // iterate through nums array
+    // check if current numeber is a zero and swap with next non-zero number
+    // return nums array
+    let [l, r] = [0, 1]
+
+    while (r < nums.length) {
+        if (nums[l] === 0 && nums[r] !== 0) {
+            nums[l] = nums[r]
+            nums[r] = 0
+            l++
+        }
+
+        if (nums[l] !== 0) l++
+        r++
+    }
+
+    return nums
+};
