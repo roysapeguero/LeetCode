b6c86aca8d67a5b4e08ef6c7b28943aa0d3e259a
Roysa Peguero Martinez <110946315+roysapeguero@users.noreply.github.com>
2024-02-19 18:02:31 -0800

    Time: 69 ms (15.91%) | Memory: 52.4 MB (7.03%) - LeetSync

--- 38-count-and-say/count-and-say.js
+++ 38-count-and-say/count-and-say.js
@@ -3,48 +3,26 @@
  * @return {string}
  */
 var countAndSay = function(n) {
-    // create a map to keep track of the step and its result
-    // iterate from 2 to n and map each value based on the previous 
-    // 
-
     let count = new Map()
     count.set(1, '1')
 
-    for (let i = 2; i <= n; i++) { // i = 3
-        let prev = count.get(i - 1) // prev = '11'
-        let j = 0
+    for (let i = 2; i <= n; i++) { 
+        let prev = count.get(i - 1) 
+        let [l, r] = [0, 0]
         let next = ''
 
-        while (j < prev.length) { 
-            // j = 0
-            let curr = prev[j] // 1
-            let k = 1 // k = 1 again
-        
-            while (prev[j + k] === curr) { // prev[1] = curr = 5
-                k++ // k = 1
+        while (r <= prev.length) { 
+            let curr = prev[l]
+            if (curr === prev[r]) {
+                r++ 
+            } else {
+                next += String(r - l) 
+                next += curr
+                l = r
+                r++
             }
-
-            let stringCount = String(k) // '1'
-            next += stringCount // next = '1'
-            next += curr // next = '11'
-            j += k // j = 1
-
-            
-        //     if (curr === prev[k]) {
-        //         k++
-        //     } else {
-        //         next += k - j
-        //         next += curr
-        //         j = k
-        //         k++
-        //     }
         }
-    count.set(i, next)
+        count.set(i, next)
     }
     return count.get(n)
 };
-
-
-// l 
-// 3322251
-//   r
