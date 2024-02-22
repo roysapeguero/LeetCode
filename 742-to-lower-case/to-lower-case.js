f38ecf496950ed97581d0abb3e387cf677828aa6
Roysa Peguero Martinez <110946315+roysapeguero@users.noreply.github.com>
2024-02-22 13:50:16 -0800

    Time: 48 ms (80.36%) | Memory: 50.9 MB (5.41%) - LeetSync

--- 742-to-lower-case/to-lower-case.js
+++ 742-to-lower-case/to-lower-case.js
@@ -3,5 +3,14 @@
  * @return {string}
  */
 var toLowerCase = function(s) {
-    return s.toLowerCase()
+    // iterate through s
+    // if the char code is uppercase change letter
+
+    for (let i in s) {
+        let charCode = s.charCodeAt(i)
+        if (charCode >= 65 && charCode <= 90) {
+            s = s.replace(s[i], String.fromCharCode(charCode + 32))
+        }
+    }
+    return s
 };
