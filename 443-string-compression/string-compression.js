d85a4fcafd72a68d4dd8dfbf69df75b8da81cc7d
Roysa Peguero Martinez <110946315+roysapeguero@users.noreply.github.com>
2024-02-27 00:36:46 -0800

    Time: 62 ms (61.11%) | Memory: 51.9 MB (32.05%) - LeetSync

--- 443-string-compression/string-compression.js
+++ 443-string-compression/string-compression.js
@@ -10,7 +10,8 @@ var compress = function(chars) {
     let length = chars.length
 
     while (r <= length) {
-        let currChar = chars[l], nextChar = chars[r]
+        let currChar = chars[l]
+        let nextChar = chars[r]
         if (nextChar !== currChar || r === length) {
             s += currChar
             if (r - l > 1) s += r - l 
